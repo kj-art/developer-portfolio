14712ca417714c191f1a6fbfdb1a63bda71379d9
Krishna <krjart@outlook.com>
2025-09-20 12:26:11 -0500

    fixed imports

--- batch_rename/core/__init__.py
+++ batch_rename/core/__init__.py
@@ -7,8 +7,8 @@ Provides extractors, converters, templates, filters, and processing logic.
 # Make key classes available at package level
 from .processor import BatchRenameProcessor
 from .config import RenameConfig, RenameResult
-from .templates import get_template, is_template_function
-from .converters import get_converter, is_converter_function
+from .built_ins.templates import get_template, is_template_function
+from .built_ins.converters import get_converter, is_converter_function
 
 __all__ = [
     'BatchRenameProcessor',
